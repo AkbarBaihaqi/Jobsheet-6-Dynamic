0e3d25608b7933ae1245ac2ce77e903b22cc12c9
Lenovo <Lenovo@LAPTOP-3ABJAA55>
2018-09-12 12:48:17 +0700

    Initial commit

---
+++ src/jobsheet6dynamic/Latihan1.java
@@ -0,0 +1,16 @@
+package jobsheet6dynamic;
+public class Latihan1 {
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas: "+identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static void main(String[] args) {
+        String identitas = "Akbar Baihaqi Putra Aji Pratama/XRPL5/4";
+        
+        tampilJudul(identitas);
+       
+    }
+    
+}
